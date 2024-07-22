d7d060f00b986efe5fffc110ed2a67064f17a5f2
rbmpsh <rbmpsh@gmail.com>
2024-07-22 14:15:30 +0800

    user12 push test

--- orderConverter.py
+++ orderConverter.py
@@ -450,3 +450,4 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO, handlers=[logging.FileHandler(logFile), logging.StreamHandler()])
     main()
     input('按Enter繼續...')
+    #
